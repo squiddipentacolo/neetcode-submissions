8bef2ace33003bb88610556b57ba24f04761c91a
Andrea Mauriello <60556126+squiddipentacolo@users.noreply.github.com>
2026-08-17 14:22:31 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let left = 0;
+        let right = s.length - 1;
+        let isPalindrome = true;
+
+        while(left < right && isPalindrome) {
+            if(!this.isAlphaNum(s[left])) {
+                left++;
+            } else if(!this.isAlphaNum(s[right])) {
+                right--;
+            } else {
+                isPalindrome = s[left].toLowerCase() === s[right].toLocaleLowerCase();
+                left++;
+                right--;
+            }
+        }
+
+        return isPalindrome;
+    }
+
+    isAlphaNum(char: string): boolean {
+        const charCode = char.charCodeAt(0);
+
+        return (charCode >= 48 && charCode <= 57 || charCode >= 65 && charCode <= 90 ||
+            charCode >= 97 && charCode <= 122)
+    }
+}
