2c6127d3c5b61c54f90bb4cc2ede3a959f4a5442
Andrea Mauriello <60556126+squiddipentacolo@users.noreply.github.com>
2026-08-19 15:48:07 +0200

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+        let mid = 0;
+
+        while(l + 1 < r) {
+            mid = Math.floor((l + r) / 2);
+            
+            if(nums[mid] < target) {
+                l = mid;
+            } else if(nums[mid] > target) {
+                r = mid;
+            } else {
+                return mid;
+            }
+
+        }
+        
+        if(r - l <= 1) {
+            if(nums[l] === target) {
+                return l;
+            } else if(nums[r] === target) {
+                return r;
+            } else {
+                return -1
+            }
+        }
+
+        return mid;
+    }
+}
